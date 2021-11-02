85469f64e8ba51031cda6729e03d5485b4327db0
Hicham Roldan <hichamrldn@gmail.com>
2021-11-02 18:16:41 +0100

    intégration des produits dans l'index

--- front/js/script.js
+++ front/js/script.js
@@ -3,4 +3,27 @@
 //--------------------------------------------
 
 
-fetch("https://localhost:3000/api/products").then((res) => console.log(res));
+fetch("http://localhost:3000/api/products")
+.then((res) => res.json())
+.then(function (datas){
+    datas.forEach(product => {
+        let items =document.getElementById('items');
+            a = document.createElement('a');
+            article = document.createElement('article');
+            article.classList.add("productCard");
+            img = document.createElement('img');
+            img.classList.add("productImage");
+            h3 = document.createElement('h3');
+            h3.classList.add('productName');
+            p = document.createElement('p');
+            p.classList.add('productDescritpion');
+
+        items.appendChild(a);
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+        
+    });
+
+});
