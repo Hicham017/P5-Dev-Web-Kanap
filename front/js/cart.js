db076d283484e6a77ac98a7feaa15913b71c15b9
Hicham Roldan <hichamrldn@gmail.com>
2021-11-30 16:39:24 +0100

    page produit terminée, ajout des produits dans le panier, tableau récapitulatif, modification et suppression page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -31,7 +31,7 @@ for (let id in articles) {
 
     section.innerHTML += 
     `
-    <article class="cart__item" data-id="{product-ID}">
+    <article class="cart__item" data-id="${id}"data-color="${color}">
 <div class="cart__item__img">
   <img src="${articles[id].imageUrl}" alt="Photographie d'un canapé">
 </div>
@@ -78,4 +78,82 @@ totalP.innerHTML = priceTotal;
 displayTotal();
 
 
+function takeInput() {
+  let input = document.getElementsByClassName('itemQuantity');
+  for (let i = 0; i < input.length; i++) {
+  
+    input[i].addEventListener('change', (e) => {
+
+     
+      let carte =  JSON.parse(
+        localStorage.getItem('panier')); //panier recup depuis local storage
+
+      let id = input[i].parentElement.parentElement.parentElement.parentElement.dataset.id; //recupère ce qu'il y a dans données
+      let color = input[i].parentElement.parentElement.parentElement.parentElement.dataset.color;
+      let quantity = input[i].value;
+
+      carte[id].colors[color] = parseInt(quantity);  //on veut que quantity soit un nb entier 
+      
+      //[] permet de recup un element dans une liste 
+      localStorage.setItem('panier', JSON.stringify(carte));    // met à j le local storage
+
+      displayTotal();
+
+    }  
+    )}};
+
+  takeInput();
+
+//-------------------------------------------------------BOUTON SUPPRIMER------------------------------------------------------------------//
+
+function btnDelete() {
+  let buttons = document.getElementsByClassName('deleteItem');//recup tous les btn
+  for (let i = 0; i < buttons.length; i++) {     
+  
+    buttons[i].addEventListener('click', (e) => {    
+
+     
+      let carte =  JSON.parse(
+        localStorage.getItem('panier')); //panier recup depuis local storage
+
+      let id = buttons[i].parentElement.parentElement.parentElement.parentElement.dataset.id; //recupère ce qu'il y a dans données
+      let color = buttons[i].parentElement.parentElement.parentElement.parentElement.dataset.color;
+      
+
+      delete carte[id].colors[color];
+      
+      //[] permet de recup un element dans une liste 
+      localStorage.setItem('panier', JSON.stringify(carte));    // met à j le local storage
+      buttons[i].parentElement.parentElement.parentElement.parentElement.remove(),
+      displayTotal();
+
+    }  
+    )}};
+
+    btnDelete();
+
+
+
+//-----------------------------------------PASSER LA COMMANDE-----------------------------------------------//
+
+//recup tous les éléments du form 1 par 1 
+//verif leur valeurs, renvoie true avec les regex, pour prenom, adresse ect ...
+//getelebyid firstname.value
+//city.value ect 
+
+//recup regex sur intenet, regular expression 
+
+
+function checkForm() {
+
+  let takeFirstName = getElementById('firstName').value;
+  let takeLastName = getElementById('lastName').value;
+  let takeAddress = getElementById('address').value;
+  let takeCity = getElementById('city').value;
+  let takeEmail = getElementById('email').value;
+
+}
+
+
+//revoir les array et les i (sac), là je suis dans du js, là dans du HTML 
 
